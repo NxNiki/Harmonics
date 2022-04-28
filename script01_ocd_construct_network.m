8e88c646b186420fd9d402869af3663652596d0e
NxNiki <michaelniki1988@gmail.com>
2022-04-28 11:37:10 -0400

    add scripts for ocd

--- script01_ocd_construct_network.m
+++ script01_ocd_construct_network.m
@@ -1,11 +1,21 @@
 % read OCD data:
 
 % correlation matrix is created with python script and results are saved as .txt files.
-% this script is not necessary.
+% this script is not necessary for adni data.
 % xin Mar 29 2022.
 
-input_dir = "/home/xin/Downloads/OCD_116ROIs_TimeSeries_Guorong_10_13/HC_ROI_signals";
-output_dir = "/home/xin/Downloads/BrainImaging_UNC/ocd_out01_correlation_matrix_hc";
+% we still need this cor ocd data as the input are .mat files.
+% xin Apr 25 2022.
+
+% input_dir = "/home/xin/Downloads/OCD_116ROIs_TimeSeries_Guorong_10_13/HC_ROI_signals";
+% output_dir = "/home/xin/Downloads/Harmonics/ocd_out01_correlation_matrix_hc";
+
+input_dir = "/home/xin/Downloads/OCD_116ROIs_TimeSeries_Guorong_10_13/OCD_ROI_signals";
+output_dir = "/home/xin/Downloads/Harmonics/ocd_out01_correlation_matrix_ocd";
+
+% input_dir = "/home/xin/Downloads/OCD_116ROIs_TimeSeries_Guorong_10_13/OCDS_ROI_signals";
+% output_dir = "/home/xin/Downloads/Harmonics/ocd_out01_correlation_matrix_ocds";
+
 
 if ~exist(output_dir, 'dir')
     mkdir(output_dir)
@@ -14,12 +24,24 @@ end
 files = dir(strcat(input_dir, '/ROISignals*.mat'));
 
 for i = 1:length(files)
+    
     fprintf([files(i).name, '\n'])
 
+    % load .mat file.
     load(strcat(files(i).folder, '/', files(i).name))
-    out_file_name = strcat('CorrelationMatrix', files(i).name(11:end));
 
-    CorrMatrix = corr(ROISignals);
-    save(strcat(output_dir, '/', out_file_name), 'CorrMatrix')
+%     CorrMatrix = corr(ROISignals);
+
+    % remove rows with nan:
+    nan_row = any(isnan(ROISignals), 2);
+    CorrMatrix = corrcoef(ROISignals(~nan_row,:));
+    
+    
+    out_file_name = strcat('ROISignals', files(i).name(11:end));
+    writematrix(ROISignals(~nan_row,:), strcat(output_dir, '/', strrep(out_file_name, '.mat', '.csv')))
+    % save correlation matrix as .csv file:
+    out_file_name = strcat('CorrelationMatrix', files(i).name(11:end));
+    writematrix(CorrMatrix, strcat(output_dir, '/', strrep(out_file_name, '.mat', '.csv')))
+    %save(strcat(output_dir, '/', out_file_name), 'CorrMatrix')
 
 end
